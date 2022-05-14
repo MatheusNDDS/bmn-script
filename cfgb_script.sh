7e220469ab9c67cd8802b5b1c7154ce7706cb9f9
Matheus Dias <matheus.d.silva112@gmail.com>
2022-05-14 02:05:25 -0300

    Update cfgb_script.sh
    Add flatpak support!

--- cfgb_script.sh
+++ cfgb_script.sh
@@ -8,39 +8,56 @@ if [ $1 = setup ]; then
 if [ $2 = "" ] ; then echo  ; exit ; fi ;
 sudo mkdir /usr/share/cfgb ; sudo mkdir /usr/share/cfgb/bundles ;
 sudo cp -r "$(pwd)/cfgb_script.sh" /bin/cfgb ; sudo chmod +x /bin/cfgb ;
-
 #setting configs variables
 sudo echo -e "
-export repo=$3
+export repo=$4
 export h=/home/$2
-export pm=$4" > /usr/share/cfgb/cfg ; echo C.F.G.B Manager instaled;
+export pm=$3" > /usr/share/cfgb/cfg ; echo C.F.G.B Manager instaled;
 exit
 fi
+
+#Setup Global variables (mini shell)
+export name=cfgb ; export cp="sudo cp -r";export rm="sudo rm -rf";export prt="echo -e" ; export pdir=/usr/share/$name ; export id=$2 ; export mkd='sudo mkdir' ; export bnd_dir=$pdir/bundles;export add_ppa="sudo add-apt-repository" ;
+
+#Start Setup
+cfg=$(cat $pdir/cfg); $cfg ; cd $bnd_dir ;
+
+#extra package managers
+if [ $1 = '-e' ]; then 
+for a in $2 $3 ; do 
+if [ $a = flatpak ] ; then
+echo "-=- [$name]: Configuring $a -=-" ; $pm install flatpak -y ;
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo ; echo "-=- [$name]: OK! -=-"
+fi
+if [ $a = snap ] ; then
+echo soom!
+fi
+done ; exit 0 ; fi ;
 
 #configurations debug
 if [ $1 = -l ]; then cat /usr/share/cfgb/cfg ; exit ; fi
 
-#Global variables (mini shell)
-export name=cfgb ; export cp="sudo cp -r";export rm="sudo rm -rf";export prt="echo -e" ; export pdir=/usr/share/$name ; export id=$2 ; export mkd='sudo mkdir' ; export bnd_dir=$pdir/bundles;export add_ppa="sudo add-apt-repository" ; export fp="flatpak install flathub"
-
-#Setup
-cfg=$(cat $pdir/cfg); $cfg ; cd $bnd_dir ;
-
-#pkg install function
 pkg_install () {
-pkgm=($pm 'install' 'update' 'upgrade');
-echo -=- [${pkgm[0]}]: Installing Packages -=-;
-echo '-=- Atualizando '${pkmg[0]}' -=-'
-sudo ${pkgm[0]} ${pkgm[2]} -y ;
-sudo ${pkgm[0]} ${pkgm[3]} -y ;
-for i in $(cat $bnd_dir/$id/packages) ; do echo -=$i=- ; sudo ${pkgm[0]} ${pkgm[1]} $i -y ; done
-echo -=- [$name]: Cooking Directories -=- ; } ;
-export -f pkg_install
+#Distro Pkgs
+if [ -e $bnd_dir/$id/packages ] ; then
+	pkgm=($pm 'install' 'update' 'upgrade');
+	echo -=- [${pkgm[0]}]: Installing Packages -=-;
+	echo '-=- Atualizando '${pkmg[0]}' -=-'
+	sudo ${pkgm[0]} ${pkgm[2]} -y ;
+	sudo ${pkgm[0]} ${pkgm[3]} -y ;
+	for i in $(cat $bnd_dir/$id/packages) ; do echo -=$i=- ; sudo ${pkgm[0]} ${pkgm[1]} $i -y ; done ; 
+	fi ;
+#Flatpaks
+	if [ -e $bnd_dir/$id/flatpaks ] ; then
+	pkgm=('flatpak' 'install' 'update' 'flathub') ;
+	echo '-=- Atualizando Flathub -=-'
+	sudo ${pkgm[0]} ${pkgm[2]} -y ;
+	for i in $(cat $bnd_dir/$id/flatpaks) ; do echo -=$i=- ; sudo ${pkgm[0]} ${pkgm[1]} ${pkgm[3]} $i -y ; done ;
+fi ;
 
 $prt '[Configuration Bundles Manager] by -=Matheus Dias=-
 ~Making desktop setups most simple possible!~
 '
-
 if [ $1 = -i ]; then 
 args=($2 $3 $4 $5 $6) ; for a in ${args[@]} ; do
 #getting a bundle
@@ -56,8 +73,11 @@ echo files: [$(ls -c $bnd_dir/$a)] -Ok
 
 #Cooking directories recipes
 $prt '-=- ['$a']: Cooking Bundle -=-'
-cd $bnd_dir/$a/ ; bash recipe
-echo '-=- '$a' Instaled -=-' ;  done ;
+cd $bnd_dir/$a/ ; pkg_install ; 
+if [ -e $bnd_dir/$a/recipe ] ; then
+echo -=- [$name]: Cooking Directories -=- ; } ;
+bash recipe ;
+fi ; echo '-=- '$a' Instaled -=-' ;  done ;
 fi
 
 #Clean bundles folder
