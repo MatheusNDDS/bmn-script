9d6235077d363a532c396166863fa63dc279dce9
Matheus Dias <matheus.d.silva112@gmail.com>
2023-11-17 14:11:59 -0300

    Add files via upload

--- cfgb.sh
+++ cfgb.sh
@@ -127,7 +127,7 @@ load_data $*
 	then
 		output 0
 		output 1
-		$prt "\n\033[01;33m[Commands]\033[00m\n--install,-i : Install bundles from repository, use -iu to update $pm repositories during installation.\n--dowload,-d : Download bundles from repository.\n--repo-update,-rU : Update repository release file, use this regularly.\n--cfgb-update,-U : Update cfgb script from repo source.\n--list-bnds,-l : List or search for bundles in repo file.\n--live-shell,-sh : Run live shell for testing cfgb functions.\n--help,-h : Print help text."
+		$prt "\n\033[01;33m[Commands]\033[00m\n --install,-i : Install bundles from repository, use -iu to update $pm repositories during installation.\n --dowload,-d : Download bundles from repository.\n --repo-update,-rU : Update repository release file, use this regularly.\n --cfgb-update,-U : Update cfgb script from repo source.\n --list-bnds,-l : List or search for bundles in repo file.\n --live-shell,-sh : Run live shell for testing cfgb functions.\n --help,-h : Print help text."
 	elif [[ $1 = '-sh' ]]
 	then
 		live_shell
@@ -139,7 +139,7 @@ load_data $*
 output(){
 	declare -A t
 	t[header]="\033[01;36m-=/Configuration Bundles Manager/=-\033[00m \n~ MatheusNDDS : https://github.com/MatheusNDDS\n"
-	t[info_header]="\033[01;33m[Properties]\033[00m\nUser: $u\nHome: $h\nPkgM: $pm\nRepo: $repo"
+	t[info_header]="\033[01;33m[Properties]\033[00m\n User: $u\n Home: $h\n PkgM: $pm\n Repo: $repo"
 	t[progress]="\033[01;35m [$2]: -=- $3 -=-\033[00m"
 	t[list]="\033[01m $2: [ $($prt $3|tr ' ' ', ') ]\033[00m "
 	t[dialogue]="\033[01m [$2]: $3\033[00m"
