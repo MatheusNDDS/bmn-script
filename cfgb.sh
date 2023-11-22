4781563328390c57809ddfa0b150492de22a6de8
Matheus Dias <matheus.d.silva112@gmail.com>
2023-11-22 12:51:53 -0300

    Add files via upload

--- cfgb.sh
+++ cfgb.sh
@@ -92,7 +92,6 @@ load_data $*
 		for i in ${args[@]:2}
 		do
 			bnd_parser $i
-			bnd_log+=($bndf)
 			if [[ $bndf = *"$file_format"* ]] #detect "tar.gz" file.
 			then
 				lc_inst=1
@@ -104,26 +103,23 @@ load_data $*
 					output -hT "Installing “$bndf” $(if [[ ! -z $bnd_flags ]];then $prt : ${bnd_flags[@]};fi)"
 					if [[ $lc_inst = 1 ]] #if "tar.gz" file detected change the download mode to import mode.
 					then
-						if [[ ! $bnd_log =~ (^|[[:space:]])$bnd_name($|[[:space:]]) ]]
-						then
-							$srm $bnd_dir/$bnd_name/ $bnd_dir/$bndf
-						fi
+						$srm $bnd_dir/$bnd_name/ $bnd_dir/$bndf
 						output -p $name "Importing “$bndf”"
 						$cp $bndf $bnd_dir/
 						output -l imported "$(ls $bnd_dir/ | grep $bndf)"
 						bndf=$bnd_name
 					else
 						cd $bnd_dir/
-						if [[ ! $bnd_log =~ (^|[[:space:]])$bnd_name($|[[:space:]]) ]]
-						then
-							$srm $bnd_dir/$bndf/ $bnd_dir/$bndf.$file_format
-						fi
+						$srm $bnd_dir/$bndf/ $bnd_dir/$bndf.$file_format
 						download $bndf 0
 					fi
 					cd $bnd_dir/
 					unpack $bndf
 					cook $bndf ${bnd_flags[@]}
-					$srm $bnd_dir/$bndf $bnd_dir/$bndf.$file_format
+					if [[ $cfgb_preserve != 1 ]]
+					then
+						$srm $bnd_dir/$bndf $bnd_dir/$bndf.$file_format
+					fi
 					lc_inst=0
 				else
 					output -e $name "“$i” bundle not found"
