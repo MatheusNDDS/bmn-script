c0a1d8d91e8f8a91fe501055ca33b48aaaea7488
Matheus Dias <matheus.d.silva112@gmail.com>
2023-11-25 13:47:46 -0300

    Add files via upload

--- bmn.sh
+++ bmn.sh
@@ -652,7 +652,7 @@ setup(){
 	$elf $cmd_srcd/$name
 #Set the init file
 	$prt "source $cmd_srcd/$name" > $init_file
-	$scat $lshrc >> $init_file
+	$prt 'export PS1="\n“\w”:$(output -d $name)' | sed '$ ! s/:/\\n/'  >> $init_file
 #Package manager autodetect
 	output -p $name "Detecting Package Manager"
 	pma -qpm 2> $log
